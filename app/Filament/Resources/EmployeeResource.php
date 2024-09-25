4dd91c390bb2415c03219fc909c75a019438e119
Duhasky <jeffersoncavpes@gmail.com>
2024-09-25 16:42:46 -0300

    MM-2: wip

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Resources;
 
+use App\Enums\{Genders, MaritalStatus, States};
 use App\Filament\Resources\EmployeeResource\RelationManagers\PhotosRelationManager;
 use App\Filament\Resources\EmployeeResource\{Pages};
+use App\Forms\Components\{MoneyInput, PhoneInput};
 use App\Models\Employee;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables\Table;
@@ -33,47 +36,76 @@ class EmployeeResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('gender')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('email')
-                    ->email()
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('phone_one')
-                    ->tel()
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('phone_two')
-                    ->tel()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('salary')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('rg')
-                    ->required()
-                    ->maxLength(20),
-                Forms\Components\TextInput::make('cpf')
-                    ->required()
-                    ->maxLength(20),
-                Forms\Components\DatePicker::make('birth_date')
-                    ->required(),
-                Forms\Components\TextInput::make('father')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('mother')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('marital_status')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('spouse')
-                    ->maxLength(255),
-                Forms\Components\DatePicker::make('hiring_date')
-                    ->required(),
-                Forms\Components\DatePicker::make('resignation_date'),
+                Section::make('Dados Pessoais')->schema([
+                    Forms\Components\TextInput::make('name')
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\Select::make('gender')
+                        ->required()
+                        ->options(Genders::class),
+                    Forms\Components\TextInput::make('email')
+                        ->email()
+                        ->maxLength(255),
+                    PhoneInput::make('phone_one')->required(),
+                    PhoneInput::make('phone_two'),
+                    MoneyInput::make('salary')
+                        ->required()
+                        ->numeric(),
+                    Forms\Components\TextInput::make('rg')
+                        ->label('RG')
+                        ->mask('99999999999999999999')
+                        ->maxLength(20),
+                    Forms\Components\TextInput::make('cpf')
+                        ->mask('999.999.999-99')
+                        ->required()
+                        ->maxLength(20),
+                    Forms\Components\DatePicker::make('birth_date')
+                        ->required(),
+                    Forms\Components\TextInput::make('father')
+                        ->maxLength(255),
+                    Forms\Components\TextInput::make('mother')
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\Select::make('marital_status')
+                        ->required()
+                        ->options(
+                            collect(MaritalStatus::cases())
+                            ->mapWithKeys(fn (MaritalStatus $type) => [$type->value => ucfirst($type->value)])
+                        ->toArray()
+                        ),
+                    Forms\Components\TextInput::make('spouse')
+                        ->maxLength(255),
+                    Forms\Components\DatePicker::make('hiring_date')->required(),
+                    Forms\Components\DatePicker::make('resignation_date'),
+                ])->columns(['sm' => 1, 'md' => 2, 'lg' => 3]),
+                Section::make(__('Address'))->relationship('address')->schema([
+                    Forms\Components\TextInput::make('zip_code')
+                        ->required()
+                        ->mask('99999-999'),
+                    Forms\Components\TextInput::make('street')
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\TextInput::make('number')
+                        ->numeric()
+                        ->minValue(0),
+                    Forms\Components\TextInput::make('complement')
+                        ->maxLength(255),
+                    Forms\Components\TextInput::make('neighborhood')
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\Select::make('city_id')
+                        ->relationship('city', 'name')
+                        ->searchable()
+                        ->preload()
+                        ->required(),
+                    Forms\Components\Select::make('state')
+                        ->options(collect(States::cases())->mapWithKeys(fn (States $status) => [
+                            $status->value => $status->value,
+                        ])->toArray())
+                        ->required()
+                        ->searchable()
+                        ->preload(),
+                ])->columns(['sm' => 1, 'md' => 3, 'lg' => 4]),
             ]);
     }
 
