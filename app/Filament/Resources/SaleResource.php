fe43b128c3016835b4e1177b82142535d4b028af
Duhasky <jeffersoncavpes@gmail.com>
2024-09-26 15:59:25 -0300

    MM-2: wip

--- app/Filament/Resources/SaleResource.php
+++ app/Filament/Resources/SaleResource.php
@@ -2,8 +2,11 @@
 
 namespace App\Filament\Resources;
 
+use App\Enums\PaymentMethod;
 use App\Filament\Resources\SaleResource\{Pages};
-use App\Models\Sale;
+use App\Forms\Components\MoneyInput;
+use App\Models\{Sale, Vehicle};
+use Filament\Forms\Components\{Section, ToggleButtons};
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables\Table;
@@ -13,10 +16,10 @@ class SaleResource extends Resource
 {
     protected static ?string $model = Sale::class;
 
-    public static function getNavigationGroup(): ?string
-    {
-        return __('Financial');
-    }
+    // public static function getNavigationGroup(): ?string
+    // {
+    //     return __('Financial');
+    // }
 
     public static function getModelLabel(): string
     {
@@ -32,47 +35,140 @@ class SaleResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\Select::make('user_id')
-                    ->label('Seller')
-                    ->relationship('user', 'name')
-                    ->required(),
-                Forms\Components\Select::make('vehicle_id')
-                    ->relationship('vehicle', 'id')
-                    ->required(),
-                Forms\Components\Select::make('client_id')
-                    ->relationship('client', 'name')
-                    ->required(),
-                Forms\Components\TextInput::make('payment_method')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('status')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\DatePicker::make('date_sale')
-                    ->required(),
-                Forms\Components\DatePicker::make('date_payment'),
-                Forms\Components\TextInput::make('discount')
-                    ->required()
-                    ->numeric()
-                    ->default(0.00),
-                Forms\Components\TextInput::make('surcharge')
-                    ->required()
-                    ->numeric()
-                    ->default(0.00),
-                Forms\Components\TextInput::make('down_payment')
-                    ->required()
-                    ->numeric()
-                    ->default(0.00),
-                Forms\Components\TextInput::make('number_installments')
-                    ->required()
-                    ->numeric()
-                    ->default(1),
-                Forms\Components\TextInput::make('reimbursement')
-                    ->numeric(),
-                Forms\Components\DatePicker::make('date_cancel'),
-                Forms\Components\TextInput::make('total')
-                    ->required()
-                    ->numeric(),
+                Section::make()->schema([
+                    Forms\Components\Select::make('user_id')
+                        ->label('Seller')
+                        ->relationship('user', 'name')
+                        ->required(),
+                    Forms\Components\Select::make('vehicle_id')
+                        ->relationship('vehicle', 'id')
+                        ->options(function () {
+                            return Vehicle::all()->mapWithKeys(function (Vehicle $vehicle) {
+                                return [
+                                    $vehicle->id => "{$vehicle->plate} - {$vehicle->model->name} ({$vehicle->year_one}/{$vehicle->year_two})", //@phpstan-ignore-line
+                                ];
+                            });
+                        })
+                        ->afterStateUpdated(function (Forms\Set $set, Forms\Get $get) {
+                            $vehicle      = \App\Models\Vehicle::find($get('vehicle_id')); //@phpstan-ignore-line
+                            $vehiclePrice = $vehicle->promotional_price ?? $vehicle->sale_price ?? 0;
+                            $discount     = $get('discount') !== "" ? $get('discount') : 0;
+                            $surcharge    = $get('surcharge') !== "" ? $get('surcharge') : 0;
+                            $total        = $vehiclePrice + $surcharge - $discount;
+                            $set('total', $total);
+                        })
+                        ->live()
+                        ->required(),
+                    Forms\Components\Select::make('client_id')
+                        ->relationship('client', 'name')
+                        ->searchable()
+                        ->options(function () {
+                            return \App\Models\Client::all()->mapWithKeys(function ($client) {
+                                return [
+                                    $client->id => "{$client->name} - {$client->client_id}", //@phpstan-ignore-line
+                                ];
+                            });
+                        })
+                        ->required(),
+                    Forms\Components\Select::make('payment_method')
+                        ->options(
+                            collect(PaymentMethod::cases())
+                                ->mapWithKeys(fn (PaymentMethod $type) => [$type->value => ucfirst($type->value)])
+                        )
+                        ->required(),
+                    Forms\Components\DatePicker::make('date_sale')
+                        ->required(),
+                    ToggleButtons::make('discount_surcharge')
+                        ->options([
+                            'surcharge' => 'Acréscimo',
+                            'discount'  => 'Desconto',
+                        ])
+                        ->label('Type')
+                        ->live()
+                        ->inline()
+                        ->afterStateUpdated(function (Forms\Set $set, Forms\Get $get) {
+                            $set('surcharge', 0); // Define surcharge como null
+                            $set('discount', 0); // Define discount como null
+                            $vehicle      = \App\Models\Vehicle::find($get('vehicle_id')); //@phpstan-ignore-line
+                            $vehiclePrice = $vehicle->promotional_price ?? $vehicle->sale_price ?? 0;
+                            $discount     = $get('discount') !== "" ? $get('discount') : 0;
+                            $surcharge    = $get('surcharge') !== "" ? $get('surcharge') : 0;
+                            $total        = $vehiclePrice + $surcharge - $discount;
+                            $set('total', $total);
+                        }),
+                    MoneyInput::make('discount')
+                        ->label('Desconto')
+                        ->live(debounce: 1000)
+                        ->hidden(fn ($get) => $get('discount_surcharge') === 'surcharge')
+                        ->required(fn (Forms\Get $get) => $get('discount_surcharge') === 'discount')
+                        ->afterStateUpdated(function (Forms\Set $set, Forms\Get $get) {
+                            $vehicle      = \App\Models\Vehicle::find($get('vehicle_id')); //@phpstan-ignore-line
+                            $vehiclePrice = $vehicle->promotional_price ?? $vehicle->sale_price ?? 0;
+                            $discount     = $get('discount') !== "" ? $get('discount') : 0;
+                            $surcharge    = $get('surcharge') !== "" ? $get('surcharge') : 0;
+                            $total        = $vehiclePrice + $surcharge - $discount;
+                            $set('total', $total);
+                        }),
+                    MoneyInput::make('surcharge')
+                        ->label('Acréscimo')
+                        ->live(debounce: 1000)
+                        ->hidden(fn ($get) => $get('discount_surcharge') === 'discount')
+                        ->required(fn (Forms\Get $get) => $get('discount_surcharge') === 'surcharge')
+                        ->afterStateUpdated(function (Forms\Set $set, Forms\Get $get) {
+                            $vehicle      = \App\Models\Vehicle::find($get('vehicle_id')); //@phpstan-ignore-line
+                            $vehiclePrice = $vehicle->promotional_price ?? $vehicle->sale_price ?? 0;
+                            $discount     = $get('discount') !== "" ? $get('discount') : 0;
+                            $surcharge    = $get('surcharge') !== "" ? $get('surcharge') : 0;
+                            $total        = $vehiclePrice + $surcharge - $discount;
+                            $set('total', $total);
+                        }),
+                    MoneyInput::make('total')
+                        ->readOnly(),
+                    ToggleButtons::make('payment_type')
+                        ->options([
+                            'in_sight' => 'À vista',
+                            'on_time'  => 'A prazo',
+                        ])
+                        ->label('Type')
+                        ->live()
+                        ->inline()
+                        ->afterStateUpdated(function (Forms\Set $set) {
+                            $set('number_installments', 1);
+                            $set('first_installment_date', now()->addMonth(1)->format('Y-m-d')); //@phpstan-ignore-line
+                        }),
+                ])->columns(['sm' => 1, 'md' => 2, 'lg' => 3]),
+                Section::make(__('Installments'))->visible(
+                    fn (Forms\Get $get) => $get('payment_type') === 'on_time'
+                )->schema([
+                    Forms\Components\TextInput::make('number_installments')
+                        ->required(fn (Forms\Get $get) => $get('payment_type') === 'on_time')
+                        ->numeric()
+                        ->live(debounce: 1000)
+                        ->default(1)
+                        ->minValue(1)
+                        ->afterStateUpdated(
+                            function (Forms\Set $set, Forms\Get $get) {
+                                $total            = $get('total');
+                                $downPayment      = $get('down_payment') !== "" ? $get('down_payment') : 0;
+                                $installmentValue = ($total - $downPayment) / ($get('number_installments') === '' ? 1 : $get('number_installments'));
+                                $set('installment_value', $installmentValue);
+                            }
+                        ),
+                    MoneyInput::make('down_payment')
+                        ->required(fn (Forms\Get $get) => $get('payment_type') === 'on_time')
+                        ->live(debounce: 1000)
+                        ->afterStateUpdated(
+                            function (Forms\Set $set, Forms\Get $get) {
+                                $total            = $get('total');
+                                $downPayment      = $get('down_payment') !== "" ? $get('down_payment') : 0;
+                                $installmentValue = ($total - $downPayment) / ($get('number_installments') === '' ? 1 : $get('number_installments'));
+                                $set('installment_value', $installmentValue);
+                            }
+                        ),
+                    MoneyInput::make('installment_value')->readOnly(),
+                    Forms\Components\DatePicker::make('first_installment')
+                        ->required(fn (Forms\Get $get) => $get('payment_type') === 'on_time'),
+                ])->columns(['sm' => 2, 'md' => 4]),
             ]);
     }
 
--- app/Filament/Resources/SaleResource/Pages/EditSale.php
+++ app/Filament/Resources/SaleResource/Pages/EditSale.php
@@ -5,6 +5,12 @@ namespace App\Filament\Resources\SaleResource\Pages;
 use App\Filament\Resources\SaleResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Support\Exceptions\Halt;
+use Filament\Support\Facades\FilamentView;
+
+use function Filament\Support\is_app_url;
+
+use Throwable;
 
 class EditSale extends EditRecord
 {
@@ -15,5 +21,80 @@ class EditSale extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $data['discount'] >= 0 ? $data['discount_surcharge']             = 'discount' : $data['discount_surcharge'] = 'surcharge';
+        $data['number_installments'] > 1 ? $data['payment_type']         = 'on_time' : $data['payment_type'] = 'in_sight';
+        $data['payment_type'] === 'on_time' ? $data['installment_value'] = $data['total'] / $data['number_installments'] : 'in_sight';
+
+        return $data;
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        $data['surcharge'] = $data['discount_surcharge'] === 'discount' ? 0 : $data['surcharge'];
+        $data['discount']  = $data['discount_surcharge'] === 'surcharge' ? 0 : $data['discount'];
+
+        $downPayment        = $data['down_payment'] ?? 0;
+        $numberInstallments = $data['number_installments'];
+        $installmentValue   = $numberInstallments > 1 ? ($data['total'] - $downPayment) / $data['number_installments'] : null;
+
+        // dd($data);
+
+        unset($data['installment_value']);
+        unset($data['first_installment']);
+        unset($data['payment_type']);
+        unset($data['discount_surcharge']);
+
+        return $data;
+    }
+
+    public function save(bool $shouldRedirect = true, bool $shouldSendSavedNotification = true): void
+    {
+        $this->authorizeAccess();
+
+        try {
+            $this->beginDatabaseTransaction();
+
+            $this->callHook('beforeValidate');
+
+            $data = $this->form->getState(afterValidate: function () {
+                $this->callHook('afterValidate');
+
+                $this->callHook('beforeSave');
+            });
+
+            $data = $this->mutateFormDataBeforeSave($data);
+
+            $this->handleRecordUpdate($this->getRecord(), $data);
+
+            $this->callHook('afterSave');
+
+            // dd($data);
+
+            $this->commitDatabaseTransaction();
+        } catch (Halt $exception) {
+            $exception->shouldRollbackDatabaseTransaction() ?
+                $this->rollBackDatabaseTransaction() :
+                $this->commitDatabaseTransaction();
+
+            return;
+        } catch (Throwable $exception) {
+            $this->rollBackDatabaseTransaction();
+
+            throw $exception;
+        }
+
+        $this->rememberData();
+
+        if ($shouldSendSavedNotification) {
+            $this->getSavedNotification()?->send();
+        }
+
+        if ($shouldRedirect && ($redirectUrl = $this->getRedirectUrl())) {
+            $this->redirect($redirectUrl, navigate: FilamentView::hasSpaMode() && is_app_url($redirectUrl));
+        }
     }
 }
