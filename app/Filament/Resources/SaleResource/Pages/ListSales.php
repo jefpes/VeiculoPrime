eab30b52b42d806afa10eb1e1342b1adfe248f06
Duhasky <jeffersoncavpes@gmail.com>
2024-10-02 14:22:30 -0300

    MM-2: wip

--- app/Filament/Resources/SaleResource/Pages/ListSales.php
+++ app/Filament/Resources/SaleResource/Pages/ListSales.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\SaleResource\Pages;
 
 use App\Filament\Resources\SaleResource;
+use App\Models\Sale;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListSales extends ListRecords
 {
@@ -13,7 +16,29 @@ class ListSales extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()->label(__('New Sale')),
+        ];
+    }
+
+    public function getDefaultActiveTab(): string | int | null
+    {
+        return 'All';
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'All'  => Tab::make(__('All')),
+            'Late' => Tab::make(__('Late'))->modifyQueryUsing(fn (Builder $query) => $query->whereHas('paymentInstallments', function ($query) {
+                // Verificar se a parcela está vencida (atrasada)
+                $query->where('due_date', '<', now())
+                      ->whereNull('payment_date'); // Se não houver data de pagamento, está em atraso
+            }))->badge(Sale::whereHas('paymentInstallments', function ($query) { //@phpstan-ignore-line
+                // Verificar se a parcela está vencida (atrasada)
+                $query->where('due_date', '<', now())
+                      ->whereNull('payment_date'); // Se não houver data de pagamento, está em atraso
+            })
+            ->count()),
         ];
     }
 }
