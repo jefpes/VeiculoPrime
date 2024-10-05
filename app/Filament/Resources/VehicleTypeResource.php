dfcc500e896642e792ea0a9bc0c083d382b05704
Duhasky <jeffersoncavpes@gmail.com>
2024-10-05 10:35:32 -0300

    MM-2: wip

--- app/Filament/Resources/VehicleTypeResource.php
+++ app/Filament/Resources/VehicleTypeResource.php
@@ -49,14 +49,6 @@ class VehicleTypeResource extends Resource
             ->columns([
                 Tables\Columns\TextColumn::make('name')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 //
