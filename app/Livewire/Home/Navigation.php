e85ffc1226498421c4fa9f7e938c307a47247ef0
Jefferson <jeffersoncavpes@gmail.com>
2024-11-27 23:08:42 -0300

    MM-15: wip

--- app/Livewire/Home/Navigation.php
+++ app/Livewire/Home/Navigation.php
@@ -3,7 +3,6 @@
 namespace App\Livewire\Home;
 
 use App\Models\Company;
-use Filament\Facades\Filament;
 use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
@@ -11,13 +10,7 @@ class Navigation extends Component
 {
     public function render(): View
     {
-        return view(
-            'livewire.home.navigation',
-            [
-                'company'    => Company::query()->find(1),
-                'loginRoute' => Filament::getPanel('admin')->getPath() . Filament::getPanel('admin')->getLoginRouteSlug(),
-            ]
-        );
+        return view('livewire.home.navigation', ['company' => Company::query()->find(1)]);
     }
 
 }
