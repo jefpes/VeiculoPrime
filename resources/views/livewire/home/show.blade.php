267d3d158c7731e3f1b0b6aa84923494f4365f5a
Jefferson <jeffersoncavpes@gmail.com>
2024-11-27 23:26:46 -0300

    MM-15: wip

--- resources/views/livewire/home/show.blade.php
+++ resources/views/livewire/home/show.blade.php
@@ -1,7 +1,6 @@
 <div x-data="{
     currentSlide: 0,
     slides: {{ $vehicle->photos->count() }},
-    showLargeImage: false,
     activeImage: ''
 }" class="space-y-8">
   <h1 class="text-3xl font-bold">{{ $vehicle->model->name . ' - ' . $vehicle->year_one.'/'.$vehicle->year_two }}</h1>
@@ -17,7 +16,7 @@
             <div class="aspect-w-16 aspect-h-9">
               <img src="{{ asset($photo->path) }}"
                 class="object-fill w-full h-full cursor-pointer max-h-[60vh] md:max-h-[50vh]"
-                @click="showLargeImage = true; activeImage = '{{ asset($photo->path) }}'">
+                activeImage = '{{ asset($photo->path) }}'">
             </div>
           </div>
           @endforeach
@@ -126,19 +125,6 @@
           </button>
         </div>
       @endif
-    </div>
-  </div>
-
-  <!-- Modal para imagem ampliada -->
-  <div x-show="showLargeImage" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75"
-    @click.away="showLargeImage = false">
-    <div class="relative max-w-4xl max-h-full">
-      <img :src="activeImage" :alt="'{{ $vehicle->model->name }}'" class="object-contain w-full h-full">
-      <button @click="showLargeImage = false" class="absolute top-4 right-4 text-white hover:text-gray-300">
-        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-        </svg>
-      </button>
     </div>
   </div>
 </div>
