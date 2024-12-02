869c7c63f03d41312dd8e3c94b0afadb25320475
Jefferson <jeffersoncavpes@gmail.com>
2024-12-02 19:43:09 -0300

    MM-15: wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
-import preset from './vendor/filament/filament/tailwind.config.preset'
-
+/** @type {import('tailwindcss').Config} */
 export default {
-    presets: [preset],
-    content: [
+  content: [
         './app/Filament/**/*.php',
         './app/Livewire/**/*.php',
         './resources/views/**/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
-    ],
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
+
