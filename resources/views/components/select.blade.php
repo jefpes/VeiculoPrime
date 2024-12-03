5d9db7d5c87e7cbe516b86d5228c981be98f553a
Jefferson <jeffersoncavpes@gmail.com>
2024-12-02 22:26:32 -0300

    MM-15: wip

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,15 +1,15 @@
 @props(['label' => null,'messages' => null])
 <div>
   @if ($label)
-    <label class="block text-sm font-medium text-gray-700 dark:text-white"> {{ __($label) }} </label>
+    <label class="block text-sm font-medium"> {{ __($label) }} </label>
   @endif
   <select
-    {{ $attributes->merge(['class' => 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']) }}>
+    {{ $attributes->merge(['class' => 'p-1 bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block']) }}>
     {{ $slot }}
   </select>
 
   @if ($messages)
-  <ul class="text-sm text-red-600 dark:text-red-400 space-y-1">
+  <ul class="text-sm text-red-600 space-y-1">
     @foreach ((array) $messages as $message)
     <li>{{ $message }}</li>
     @endforeach
--- resources/views/livewire/home/index.blade.php
+++ resources/views/livewire/home/index.blade.php
@@ -22,11 +22,11 @@
             <div class="mt-6 space-y-4">
                 <div class="flex gap-x-2">
                     <button wire:click="clearFilters"
-                        class="flex-1 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                        class="flex-1 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
                         <span>{{ __('Clear Filters') }}</span>
                     </button>
                     <button @click="showFilters = false"
-                        class="flex-0 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                        class="flex-0 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
                         <span>{{ __('Close') }}</span>
                     </button>
                 </div>
