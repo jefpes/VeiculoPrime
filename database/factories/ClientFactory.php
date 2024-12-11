afb86df8ee493c99b607e38f1c1c39e519b00f46
Jefferson <jeffersoncavpes@gmail.com>
2024-12-11 12:04:08 -0300

    MM-26: clientfactory refactor

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -3,8 +3,8 @@
 namespace Database\Factories;
 
 use App\Enums\{Genders, MaritalStatus};
-use App\Models\{Client};
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Model;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Client>
@@ -37,21 +37,25 @@ class ClientFactory extends Factory
         ];
     }
 
-    public function withAddress()
+    public function withAddress(int $count = 1)
     {
-        return $this->afterCreating(function (Client $client) {
-            $client->addresses()->create(array_merge(
-                AddressFactory::new()->definition()
-            ));
+        return $this->afterCreating(function (Model $model) use ($count) {
+            for ($i = 0; $i < $count; $i++) {
+                $model->addresses()->create(array_merge(
+                    AddressFactory::new()->definition()
+                ));
+            }
         });
     }
 
-    public function withAffiliate()
+    public function withAffiliate(int $count = 1)
     {
-        return $this->afterCreating(function (Client $client) {
-            $client->affiliates()->create(array_merge(
-                AffiliateFactory::new()->definition()
-            ));
+        return $this->afterCreating(function (Model $model) use ($count) {
+            for ($i = 0; $i < $count; $i++) {
+                $model->affiliates()->create(array_merge(
+                    AffiliateFactory::new()->definition()
+                ));
+            }
         });
     }
 }
