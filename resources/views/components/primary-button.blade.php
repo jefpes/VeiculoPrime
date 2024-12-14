64e37850a6976de35e81bc00b8350c859dd2eaae
Jefferson <jeffersoncavpes@gmail.com>
2024-12-14 16:47:28 -0300

    MM-26: wip

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,4 +1,10 @@
-<button style="{{ 'background-color:' . App\Models\Company::query()->first()->btn_1_color . ';color:' . App\Models\Company::query()->first()->btn_1_text_color }}"
-    {{ $attributes->merge(['class' => 'flex items-center space-x-2 px-4 py-2 rounded-md transition-colors duration-200']) }}>
+@php
+$company = App\Models\Company::query()->first();
+$btnColor = $company->btn_1_color ?? '#000';
+$btnTextColor = $company->btn_1_text_color ?? '#fff';
+@endphp
+
+<button style="background-color: {{ $btnColor }}; color: {{ $btnTextColor }};" {{ $attributes->merge(['class' => 'flex
+    items-center space-x-2 px-4 py-2 rounded-md transition-colors duration-200']) }}>
     {{ $slot }}
 </button>
--- resources/views/components/secondary-button.blade.php
+++ resources/views/components/secondary-button.blade.php
@@ -1,4 +1,10 @@
-<button style="{{ 'background-color:' . App\Models\Company::query()->first()->btn_2_color . ';color:' . App\Models\Company::query()->first()->btn_2_text_color }}"
-    {{ $attributes->merge(['class' => 'flex-0 focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2']) }}>
+@php
+$company = App\Models\Company::query()->first();
+$btnColor = $company->btn_2_color ?? '#000';
+$btnTextColor = $company->btn_2_text_color ?? '#fff';
+@endphp
+
+<button style="background-color: {{ $btnColor }}; color: {{ $btnTextColor }};"
+{{ $attributes->merge(['class' => 'flex-0 focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2']) }}>
     {{ $slot }}
 </button>
