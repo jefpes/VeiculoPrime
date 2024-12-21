f634109dd0454b29a39c08fc70eceb5a48b41c6f
Jefferson <jeffersoncavpes@gmail.com>
2024-12-21 15:30:36 -0300

    MM-33: Refactor Affiliate model: Add property annotations for improved code clarity

--- app/Models/Affiliate.php
+++ app/Models/Affiliate.php
@@ -6,6 +6,17 @@ use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Affiliate
+ * @property string $id
+ * @property string $type
+ * @property string $name
+ * @property string $phone
+ * @property string $affiliatable_id
+ * @property string $affiliatable_type
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ */
 class Affiliate extends Model
 {
     use HasFactory;
