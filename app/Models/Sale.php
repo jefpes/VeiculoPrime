ffc07b790d3f056e72e90d57267d7d9861343f05
Jefferson <jeffersoncavpes@gmail.com>
2024-12-21 18:48:13 -0300

    MM-38: Refactor Sale model to replace user_id with seller_id for improved clarity and relationship mapping

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -8,20 +8,20 @@ use Illuminate\Database\Eloquent\Relations\{BelongsTo, HasMany};
 /**
  * Class Sale
  *
- * @property \App\Models\User $user
  * @property \App\Models\Vehicle $vehicle
  * @property \App\Models\People $client
+ * @property \App\Models\People $seller
  * @property \App\Models\PaymentInstallment $paymentInstallments
  *
- * @method \App\Models\User user()
  * @method \App\Models\Vehicle vehicle()
  * @method \App\Models\People client()
+ * @method \App\Models\People seller()
  * @method \App\Models\PaymentInstallment paymentInstallments()
  *
  * @property string $id
- * @property string $user_id
  * @property string $vehicle_id
  * @property string $client_id
+ * @property string $seller_id
  * @property \Illuminate\Support\Carbon $date_sale
  * @property \Illuminate\Support\Carbon $date_payment
  * @property string $status
@@ -43,9 +43,9 @@ class Sale extends BaseModel
 
     protected $fillable = [
         'tenant_id',
-        'user_id',
         'vehicle_id',
         'client_id',
+        'seller_id',
         'payment_method',
         'status',
         'date_sale',
@@ -61,11 +61,6 @@ class Sale extends BaseModel
         'total_with_interest',
     ];
 
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
-
     public function vehicle(): BelongsTo
     {
         return $this->belongsTo(Vehicle::class);
@@ -74,11 +69,15 @@ class Sale extends BaseModel
     public function client(): BelongsTo
     {
         return $this->belongsTo(People::class, 'client_id');
-        // ->whereHas('client', fn ($query) => $query->where('is_active', true));
+    }
+
+    public function seller(): BelongsTo
+    {
+        return $this->belongsTo(People::class, 'seller_id');
     }
 
     public function paymentInstallments(): HasMany
     {
-        return $this->hasMany(PaymentInstallment::class);
+        return $this->hasMany(PaymentInstallment::class, 'sale_id');
     }
 }
