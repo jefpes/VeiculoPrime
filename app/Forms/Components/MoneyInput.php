6de637263d9ad7459407c86729ecd720bb220fb5
Jefferson <jeffersoncavpes@gmail.com>
2024-12-26 11:03:50 -0300

    MM-45: Refactor MoneyInput component to extend Money class and remove unnecessary setup

--- app/Forms/Components/MoneyInput.php
+++ app/Forms/Components/MoneyInput.php
@@ -2,22 +2,8 @@
 
 namespace App\Forms\Components;
 
-use Filament\Forms\Components\{TextInput};
+use Leandrocfe\FilamentPtbrFormFields\Money;
 
-class MoneyInput extends TextInput
+class MoneyInput extends Money
 {
-    protected string $view = 'filament-forms::components.text-input';
-
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        $this->prefix('R$')
-            ->minValue(0)
-            ->extraAlpineAttributes([
-                'x-mask:dynamic' => '$money($input, ".", "")',
-            ])->extraInputAttributes([
-                'style' => 'letter-spacing: 0.2em;',
-            ]);
-    }
 }
