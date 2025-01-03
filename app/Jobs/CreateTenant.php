da9fdfd417aafb38327135d85992501f6a4752a2
Jefferson <jeffersoncavpes@gmail.com>
2025-01-03 13:49:18 -0300

    63-adm-tenants: feat: enhance tenant creation process with role assignment and policy implementation for brand management

--- app/Jobs/CreateTenant.php
+++ app/Jobs/CreateTenant.php
@@ -2,48 +2,47 @@
 
 namespace App\Jobs;
 
-use App\Models\{Ability, Settings, Store, Tenant, User};
+use App\Models\{Ability, Role, Settings, Store, Tenant, User};
 use Database\Seeders\{AbilitySeeder, AccessorySeeder, BrandSeeder, ExtraSeeder, VehicleModelSeeder, VehicleTypeSeeder};
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Str;
 
 class CreateTenant implements ShouldQueue
 {
     use Queueable;
 
-    /**
-     * Create a new job instance.
-     */
     public function __construct(protected Tenant $tenant)
     {
     }
 
-    /**
-     * Execute the job.
-     */
     public function handle(): void
     {
         $this->tenant->run(function () {
-            $user = User::query()->create(['name' => $this->tenant->name, 'email' => $this->tenant->email, 'password' => $this->tenant->password]);
+            collect([
+                AbilitySeeder::class,
+                BrandSeeder::class,
+                VehicleTypeSeeder::class,
+                VehicleModelSeeder::class,
+                AccessorySeeder::class,
+                ExtraSeeder::class,
+            ])->each(fn ($seeder) => (new $seeder())->run());
 
-            Store::query()->create(['name' => 'Loja 1', 'slug' => 'loja-1'])->users()->attach($user->id);
+            $user        = User::query()->create(['name' => $this->tenant->name, 'email' => $this->tenant->email, 'password' => $this->tenant->password]);
+            $maintenance = User::query()->create(['name' => 'maintenance', 'email' => 'maintenance@admin.com', 'password' => 'maintenance']);
 
-            (new AbilitySeeder())->run();
+            $store = Store::query()->create(['name' => $this->tenant->name, 'slug' => Str::slug($this->tenant->name)]);
+            $store->users()->attach([$user->id, $maintenance->id]);
 
             Settings::query()->create(['name' => $this->tenant->name]);
 
-            $role = $user->roles()->create([
-                'name'      => 'master',
-                'hierarchy' => 0,
-            ]);
+            $role = Role::query()->create(['name' => 'master', 'hierarchy' => 0]);
+            $role->users()->attach($maintenance->id);
+            $role->abilities()->sync(Ability::query()->pluck('id')->toArray());
 
-            $role->abilities()->sync(Ability::query()->pluck('id')->toArray()); //@phpstan-ignore-line
-
-            (new BrandSeeder())->run();
-            (new VehicleTypeSeeder())->run();
-            (new VehicleModelSeeder())->run();
-            (new AccessorySeeder())->run();
-            (new ExtraSeeder())->run();
+            $role = Role::query()->create(['name' => 'admin', 'hierarchy' => 1]);
+            $role->users()->attach($user->id);
+            $role->abilities()->sync(Ability::query()->pluck('id')->toArray());
         });
     }
 }
