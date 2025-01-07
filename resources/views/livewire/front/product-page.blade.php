c4bf4f3b9ccf33a729be8a31c0c87b4fdcbae29e
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2025-01-07 11:11:11 -0300

    add new vehicle icons

--- resources/views/livewire/front/product-page.blade.php
+++ resources/views/livewire/front/product-page.blade.php
@@ -5,15 +5,17 @@
         <x-vehicle.header :vehicle="$vehicle"/>
         <x-vehicle.title :vehicle="$vehicle"/>
 
-        <x-vehicle.header-details :vehicle="$vehicle"/>
+        {{--        <x-vehicle.header-details :vehicle="$vehicle"/>--}}
 
         <x-vehicle.location :vehicle="$vehicle"/>
 
         <x-vehicle.price :vehicle="$vehicle" :paymentMethods="$paymentMethods"/>
 
-        <x-vehicle.about :vehicle="$vehicle"/>
-
         <x-vehicle.characteristics :vehicle="$vehicle"/>
+
+        <x-vehicle.accessories :vehicle="$vehicle"/>
+
+        <x-vehicle.extras :vehicle="$vehicle"/>
 
         <x-vehicle.similar-vehicles :vehicles="$similarVehicles"/>
     </section>
