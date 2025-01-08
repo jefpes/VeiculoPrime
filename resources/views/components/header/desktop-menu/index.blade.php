38e1dbc94ba327a0d26ac44849e285e57021abad
Jefferson <jeffersoncavpes@gmail.com>
2025-01-08 14:15:17 -0300

    66-new-front: feat: update Portuguese translations and enhance desktop menu links

--- resources/views/components/header/desktop-menu/index.blade.php
+++ resources/views/components/header/desktop-menu/index.blade.php
@@ -1,11 +1,13 @@
 <ul class="hidden items-center gap-4 sm:flex">
-    <x-header.desktop-menu.link href="#" text="Link With Bold" :bold="true" />
+    <x-header.desktop-menu.link href="{{ route('index') }}" text="Home page" :bold="true" />
+    <x-header.desktop-menu.link href="{{ route('vehicles') }}" text="Vehicles" :bold="true" />
+    <x-header.desktop-menu.link href="{{ route('about') }}" text="About" :bold="true" />
 
-    <x-header.desktop-menu.dropdown text="Dropdown Menu">
+    {{-- <x-header.desktop-menu.dropdown text="Dropdown Menu">
         <x-header.desktop-menu.dropdown.link href="#" text="Dropdown Link 1" />
         <x-header.desktop-menu.dropdown.link href="#" text="Dropdown Link 2" />
         <x-header.desktop-menu.dropdown.link href="#" text="Dropdown Link 3" />
-    </x-header.desktop-menu.dropdown>
+    </x-header.desktop-menu.dropdown> --}}
 
     <!-- CTA Button -->
     <x-header.desktop-menu.button text="Login" href="{{ route('filament.admin.auth.login') }}" />
