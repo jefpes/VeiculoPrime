a36cf98bad9f57be7c9653ac83db664d6b8b3914
Jefferson <jeffersoncavpes@gmail.com>
2025-01-09 13:44:56 -0300

    66-new-front: fix: update checkbox styles for improved accessibility and consistency

--- resources/views/components/vehicles/filters/checkbox.blade.php
+++ resources/views/components/vehicles/filters/checkbox.blade.php
@@ -3,7 +3,7 @@
 <div class="inline-flex">
     <div class="flex items-center mb-4">
         <input id="checkbox-{{ $name }}" type="checkbox" wire:model.live="selectedBrands" :key="'brand-'.$name"
-            value="{{ $name }}" class="w-4 h-4 border-gray-300 rounded focus:ring-2">
+            value="{{ $name }}" class="w-4 h-4 rounded focus:ring-2 text-[var(--f-neutral-600)] border-[var(--f-neutral-300)] bg-[var(--f-neutral-50)]">
         <label for="checkbox-{{ $name }}" class="ms-2 text-sm font-medium">{{ $name }}</label>
     </div>
 </div>
