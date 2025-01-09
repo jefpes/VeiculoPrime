beb82e83e1a1ed5b83c6be6b5e5e18a34b8e6fb5
Jefferson <jeffersoncavpes@gmail.com>
2025-01-09 09:23:27 -0300

    66-new-front: fix: logo size

--- resources/views/components/header/brand-logo.blade.php
+++ resources/views/components/header/brand-logo.blade.php
@@ -6,7 +6,7 @@
     @if($settings->logo)
         <img src="{{ image_path($settings->logo) }}"
              alt="Application Logo"
-             class="max-w-[150px]"
+             class="max-w-[15rem] -my-2"
         />
     @else
         <h1 class="text-lg font-bold text-[var(--f-neutral-900)]">
