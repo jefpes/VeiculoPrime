b3217e77407e54b33b77d4cf8d312c17be89761d
Jefferson <jeffersoncavpes@gmail.com>
2025-01-09 20:36:16 -0300

    66-new-front: feat: update vehicle price component layout; add WhatsApp link for store contact

--- resources/views/components/vehicle/price.blade.php
+++ resources/views/components/vehicle/price.blade.php
@@ -4,7 +4,7 @@
      x-data="{modalIsOpen: false}">
     <div>
         @if($vehicle->promotional_price && $vehicle->promotional_price != '0.00')
-            <div class="flex gap-x-2">
+            <div class="md:flex gap-x-2">
                 <p class="text-[var(--f-text-variant-6)] line-through" >
                     R$ {{ number_format($vehicle->sale_price, 2, ',', '.') }}
                 </p>
@@ -19,14 +19,16 @@
             </p>
         @endif
 
-        <button @click="modalIsOpen = true"
-                class="font-medium text-black underline-offset-2 underline focus:outline-none mt-2">
+
+    </div>
+    <div>
+        {{-- @todo possivel simulacao de financiamento
+        <button @click="modalIsOpen = true" class="font-medium text-black underline-offset-2 underline focus:outline-none mt-2">
             {{ trans('Payment methods') }}
         </button>
 
-        <x-vehicle.payment-methods :methods="$paymentMethods"/>
-    </div>
-    <div>
+        <x-vehicle.payment-methods :methods="$paymentMethods" />
+        --}}
         <a href="{{ $vehicle->store->phones->random()->gerarLinkWhatsApp() }}" target="_blank" class="bg-[var(--f-secondary-color)] text-[var(--f-text-variant-1)] font-semibold px-6 py-2 rounded-lg focus:outline-none">
             {{ trans('Buy') }}
         </a>
