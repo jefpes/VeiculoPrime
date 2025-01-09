be1b0e0d1c8e0678656abcf20e12694b153b1ea8
Jefferson <jeffersoncavpes@gmail.com>
2025-01-09 13:29:19 -0300

    66-new-front: feat: refactor vehicle filters to use select-option component for improved readability and maintainability

--- resources/views/components/vehicles/filters/index.blade.php
+++ resources/views/components/vehicles/filters/index.blade.php
@@ -1,9 +1,9 @@
 <div class="flex flex-col gap-x-2 gap-y-4">
 
     <x-vehicles.filters.select id="select-type" label="Type" wire:model.live="vehicle_type">
-        <option value="" selected> {{ __('Select one') }}</option>
+        <x-vehicles.filters.select-option label="Select one" value="" selected/>
         @foreach ($this->types as $data)
-        <option value="{{ $data->name }}">{{ $data->name }}</option>
+        <x-vehicles.filters.select-option label="{{ $data->name }}" value="{{ $data->name }}"/>
         @endforeach
     </x-vehicles.filters.select>
 
@@ -17,21 +17,21 @@
     </div>
 
     <x-vehicles.filters.select id="select-order" label="Order" wire:model.live="order">
-        <option value="asc"> {{ __('Growing') }}</option>
-        <option value="desc">{{ __('Descending') }}</option>
+        <x-vehicles.filters.select-option label="Growing" value="asc"/>
+        <x-vehicles.filters.select-option label="Descending" value="desc"/>
     </x-vehicles.filters.select>
 
     <div class="flex gap-2 mt-2">
         <x-vehicles.filters.select id="select-year-ini" label="Year initial" wire:model.live="year_ini" >
-            <option value="" selected> {{ __('Select one') }}</option>
-            @foreach ($this->years as $y)
-                <option value="{{ $y->year_one }}">{{ $y->year_one }}</option>
+            <x-vehicles.filters.select-option label="Select one" value="" selected/>
+            @foreach ($this->years as $data)
+                <x-vehicles.filters.select-option label="{{ $data->year_one }}" value="{{ $data->year_one }}"/>
             @endforeach
         </x-vehicles.filters.select>
         <x-vehicles.filters.select id="select-year-final" label="Year final" wire:model.live="year_end" >
-            <option value="" selected> {{ __('Select one') }}</option>
-            @foreach ($this->years as $y)
-                <option value="{{ $y->year_one }}">{{ $y->year_one }}</option>
+            <x-vehicles.filters.select-option label="Select one" value="" selected/>
+            @foreach ($this->years as $data)
+                <x-vehicles.filters.select-option label="{{ $data->year_one }}" value="{{ $data->year_one }}" />
             @endforeach
         </x-vehicles.filters.select>
     </div>
