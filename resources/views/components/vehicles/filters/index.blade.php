776e7d1069d982552726eb7c546451ba26141c8d
Jefferson <jeffersoncavpes@gmail.com>
2025-01-08 22:18:20 -0300

    66-new-front: feat: enhance vehicle filters with brand selection and improved layout

--- resources/views/components/vehicles/filters/index.blade.php
+++ resources/views/components/vehicles/filters/index.blade.php
@@ -1,16 +1,26 @@
-<div class="flex flex-col gap-2">
+<div class="flex flex-col gap-x-2 gap-y-4">
+
+    <x-vehicles.filters.select id="select-type" label="Type" wire:model.live="vehicle_type">
+        <option value="" selected> {{ __('Select one') }}</option>
+        @foreach ($this->types as $data)
+        <option value="{{ $data->name }}">{{ $data->name }}</option>
+        @endforeach
+    </x-vehicles.filters.select>
+
+    <div class="space-y-2">
+        <h3 class="relative flex w-full flex-col gap-1 text-[var(--f-neutral-600)]">{{ __('Brands') }}</h3>
+        <div class="grid grid-cols-2 gap-2">
+            @foreach ($this->brands as $data)
+                <x-vehicles.filters.checkbox name="{{ $data->name }}" value="{{ $data->name }}" />
+            @endforeach
+        </div>
+    </div>
 
     <x-vehicles.filters.select id="select-order" label="Order" wire:model.live="order">
         <option value="asc"> {{ __('Growing') }}</option>
         <option value="desc">{{ __('Descending') }}</option>
     </x-vehicles.filters.select>
 
-    <x-vehicles.filters.select id="select-type" label="Type" wire:model.live="vehicle_type">
-        <option value="" selected> {{ __('Select one') }}</option>
-        @foreach ($this->types as $data)
-            <option value="{{ $data->name }}">{{ $data->name }}</option>
-        @endforeach
-    </x-vehicles.filters.select>
     <div class="flex gap-2 mt-2">
         <x-vehicles.filters.select id="select-year-ini" label="Year initial" wire:model.live="year_ini" >
             <option value="" selected> {{ __('Select one') }}</option>
