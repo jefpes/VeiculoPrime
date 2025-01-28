ad36cdd05f742beac6012160a1cf04e4c44e8a0f
Jefferson <jeffersoncavpes@gmail.com>
2025-01-28 11:14:03 -0300

    91-adjusts: feat: Refactor vehicle card component layout for improved price display and store link accessibility

--- resources/views/components/vehicles-home/card.blade.php
+++ resources/views/components/vehicles-home/card.blade.php
@@ -14,38 +14,40 @@
                     <span class="">{{ strtoupper($vehicle->model->brand->name) }} {{strtoupper($vehicle->model->name)}}</span>
                 </h4>
 
-                <p class="mb-2">
-                    <span class="text-[var(--f-text-variant-6)] border-r border-[var(--f-text-variant-3)] pr-2 tracking-wide">{{ $vehicle->year_one }}/{{ $vehicle->year_two }}</span>
-                    <span class="text-[var(--f-text-variant-6)] pl-2">{{ number_format($vehicle->km, 0, '', '.')  }} KM</span>
+                <p class=" justify-between flex">
+                    <span class="text-[var(--f-text-variant-6)] tracking-wide">{{ $vehicle->year_one }}/{{ $vehicle->year_two }}</span>
+                    <span class="text-[var(--f-text-variant-6)] ">{{ number_format($vehicle->km, 0, '', '.')  }} KM</span>
                 </p>
+                <div class="flex gap-4">
+                    @if($vehicle->promotional_price)
+                    <div class="flex gap-2">
+                        <p class="text-[var(--f-text-variant-6)] line-through text-sm">
+                            R$ {{ number_format($vehicle->sale_price, 2, ',', '.') }}
+                        </p>
 
-                @if($vehicle->promotional_price)
-                <div class="flex gap-2">
-                    <p class="text-[var(--f-text-variant-6)] line-through text-sm">
-                        R$ {{ number_format($vehicle->sale_price, 2, ',', '.') }}
-                    </p>
-
-                    <p class="text-lg mb-auto font-bold text-[var(--f-secondary-color)]">
-                        R$ {{ number_format($vehicle->promotional_price, 2, ',', '.') }}
-                    </p>
+                        <p class="text-lg font-bold text-[var(--f-secondary-color)]">
+                            R$ {{ number_format($vehicle->promotional_price, 2, ',', '.') }}
+                        </p>
+                    </div>
+                    @else
+                        <p class="text-lg font-bold text-[var(--f-text-variant-6)]">
+                            R$ {{ number_format($vehicle->sale_price, 2, ',', '.') }}
+                        </p>
+                    @endif
                 </div>
-                @else
-                    <p class="text-lg mb-auto font-bold text-[var(--f-text-variant-6)]">
-                        R$ {{ number_format($vehicle->sale_price, 2, ',', '.') }}
-                    </p>
-                @endif
             </a>
-
-            <a href="{{ $vehicle->store->gerarLinkGoogleMaps() }}"
-                class="text-[var(--f-text-variant-6)] cursor-pointer p-2 rounded-lg flex items-center justify-center gap-2 hover:text-blue-700 hover:underline"
-                target="_blank">
-                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                    <path strokeLinecap="round" strokeLinejoin="round"
-                        d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-                </svg>
-                <span > {{ $vehicle->store->city }} - {{ $vehicle->store->state }} </span>
-            </a>
+            <div class="flex border-t items-center justify-center">
+                <a href="{{ $vehicle->store->gerarLinkGoogleMaps() }}"
+                    class="text-[var(--f-text-variant-6)] my-2 cursor-pointer flex  hover:text-blue-700 hover:underline"
+                    target="_blank">
+                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                        <path strokeLinecap="round" strokeLinejoin="round"
+                            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                    </svg>
+                    <span > {{ $vehicle->store->city }} - {{ $vehicle->store->state }} </span>
+                </a>
+            </div>
         </div>
     </div>
 </div>
